b81e73ca0464e390fd1d4c56e412377421d3c297
Paweł Świątkowski <katafrakt@vivaldi.net>
2020-12-11 20:41:09 +0100

    day 11, part 1: 18x faster

--- 11/solution.cr
+++ 11/solution.cr
@@ -5,14 +5,17 @@ struct Seat
   end
 end
 
-seating = [] of Seat
+alias Coord = Tuple(Int32, Int32)
+
+seating = {} of Coord => Seat
 
 fname = "input"
 File.open(fname) do |file|
   file.each_line.with_index do |line, lidx|
     line.split("").each_with_index do |val, vidx|
       if val == "L"
-        seating << Seat.new(lidx, vidx, false)
+        coord = Coord.new(lidx, vidx)
+        seating[coord] = Seat.new(lidx, vidx, false)
       end
     end
   end
@@ -20,16 +23,21 @@ end
 
 def calculate_seating_hash(seating)
   seating
-    .map { |s| [s.x, s.y, s.occupied.to_s].join("-") }
+    .map {|_, s| [s.x, s.y, s.occupied.to_s].join("-").as(String) }
     .join(" ")
 end
 
+DIRECTIONS = [
+  [1,1], [1,0], [1,-1],
+  [0,1], [0,-1],
+  [-1,1], [-1,0], [-1,-1]
+]
+
 def get_adjacent_seats(seating, seat)
-  seating.select do |candidate|
-    candidate.x >= seat.x - 1 && candidate.x <= seat.x + 1 &&
-      candidate.y >= seat.y - 1 && candidate.y <= seat.y + 1 &&
-      [candidate.x, candidate.y] != [seat.x, seat.y]
-  end
+  DIRECTIONS.map do |dir|
+    coord = Coord.new(seat.x + dir[0], seat.y + dir[1])
+    seating[coord]?
+  end.compact
 end
 
 def solution1(seating)
@@ -38,15 +46,16 @@ def solution1(seating)
     new_hash = calculate_seating_hash(seating)
     break if new_hash == current_hash
 
-    new_seating = [] of Seat
-    seating.each do |seat|
+    new_seating = {} of Coord => Seat
+    seating.each do |_, seat|
       adjacent = get_adjacent_seats(seating, seat)
+      coord = Coord.new(seat.x, seat.y)
       if seat.occupied && adjacent.count { |s| s.occupied } >= 4
-        new_seating << Seat.new(seat.x, seat.y, false)
+        new_seating[coord] = Seat.new(seat.x, seat.y, false)
       elsif !seat.occupied && adjacent.all? { |s| !s.occupied }
-        new_seating << Seat.new(seat.x, seat.y, true)
+        new_seating[coord] = Seat.new(seat.x, seat.y, true)
       else
-        new_seating << seat
+        new_seating[coord] = seat
       end
     end
 
@@ -55,7 +64,8 @@ def solution1(seating)
     current_hash = new_hash
   end
 
-  puts seating.count { |s| s.occupied }
+  puts ""
+  puts seating.count { |_,s| s.occupied }
 end
 
 solution1(seating)
