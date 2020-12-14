679a329550095744cd943b6fe082aa7da2b67481
Paweł Świątkowski <katafrakt@vivaldi.net>
2020-12-14 08:14:29 +0100

    day 13, part 2 - working

--- 13/solution_2.d
+++ 13/solution_2.d
@@ -1,50 +1,73 @@
-import std.math, std.conv, std.bigint, std.algorithm, std.array, std.string, std.stdio;
+import std.bigint, std.algorithm, std.conv, std.array, std.stdio, std.string;
 
-struct Result {
-  BigInt offset;
-  BigInt cycle;
-}
+BigInt inverse(BigInt a, int m) {
+  if(m == 0) return BigInt(0);
+  int m0 = m;
+  BigInt y = 0, x = 1;
+  if(m == 1) return BigInt(0);
 
-Result* find_next_cycle(Result* previous, BigInt next, int index) {
-  writeln("offset=", previous.offset, " cycle=", previous.cycle, " current=", next);
-  BigInt candidate = (previous.offset/next) * next - previous.offset - index;
-  BigInt helper = candidate;
-  long i = 0; // helper counter to avoid bigint addition in the loop
-  while(true) {
-    helper = helper % previous.cycle; 
-    if(helper == 0) break;
-    i++;
-    helper += next;
+  while(a > 1) {
+    BigInt q = a / m;
+    BigInt t = m;
+    m = a % m;
+    a = t;
+    t = y.to!BigInt;
+
+    y = x - q * y;
+    x = t;
   }
-  return new Result(candidate + (next*i) + previous.offset, previous.cycle * BigInt(next));
-}
 
-BigInt find_min_common_cycle(int[] cycles) {
-  auto big_cycles = cycles.map!(a => BigInt(a));
-  auto result = new Result(BigInt(0), big_cycles[0]);
-  for(int i = 1; i < cycles.length; i++) {
-    if(cycles[i] == 0) continue;
-    result = find_next_cycle(result, big_cycles[i], i);
-  }
-  return result.offset;
-}
-
-void main() {
-   auto file = File("input");
-   file.readln(); // first line, discard
-   auto buses = file.readln();
-   buses = buses.tr("x", "0");
-   auto input = buses.split(",").map!(to!int).array;
-   auto result = find_min_common_cycle(input);
-   writeln(result);
+  if(x < 0) x = x + m0;
+  return x;
 }
 
 unittest {
-  assert(find_min_common_cycle([5,7,13]) == 440);
-  assert(find_min_common_cycle([17,0,13,19]) == 3417);
-  assert(find_min_common_cycle([7,13,0,0,59,0,31,19]) == 1_068_781);
-  assert(find_min_common_cycle([67,7,59,61]) == 754_018);
-  assert(find_min_common_cycle([67,0,7,59,61]) == 779_210);
-  assert(find_min_common_cycle([67,7,0,59,61]) == 1_261_476);
-  assert(find_min_common_cycle([1789,37,47,1889]) == 1_202_161_486);
+  assert(inverse(BigInt(91), 5) == 1);
+  assert(inverse(BigInt(65), 7) == 4);
+}
+
+BigInt chineseRemainder(BigInt[] offsets, BigInt[] nums) {
+  BigInt n = fold!((a,b) => a * b)(nums);
+  BigInt[] ys, zs;
+  ys.length = nums.length;
+  zs.length = nums.length;
+
+  for(int i = 0; i < nums.length; i++) {
+    ys[i] = n/nums[i];
+    zs[i] = inverse(ys[i], nums[i].to!int);
+  }
+
+  BigInt sum = 0;
+  for(int i = 0; i < nums.length; i++) {
+    sum += offsets[i] * ys[i] * zs[i];
+  }
+  return sum % n;
+}
+
+BigInt calcChinese(int[] nums) {
+  BigInt[] offsets;
+  const ulong m = nums.length;
+  offsets.length = m;
+  offsets[0] = 0;
+  for(int i = 1; i < m; i++) {
+    offsets[i] = BigInt(nums[i]-i);
+  }
+  BigInt[] big_nums = nums.map!(to!BigInt).array;
+  return chineseRemainder(offsets, big_nums);
+}
+
+unittest {
+  assert(calcChinese([5,7,13]) == 440);
+  assert(calcChinese([67,7,59,61]) == 754_018);
+  assert(calcChinese([17,1,13,19]) == 3417);
+}
+
+void main() {
+  auto file = File("input");
+   file.readln(); // first line, discard
+   auto buses = file.readln();
+   buses = buses.tr("x", "1");
+   auto input = buses.split(",").map!(to!int).array;
+   auto result = calcChinese(input);
+   writeln(result);
 }
