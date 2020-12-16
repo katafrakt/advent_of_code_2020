deb42e334700d5fd6b9eadc122bf34a264e4f55a
Paweł Świątkowski <katafrakt@vivaldi.net>
2020-12-16 18:43:57 +0100

    day 16, part 2

--- 16/solution.rb
+++ 16/solution.rb
@@ -1,7 +1,7 @@
 fields = {}
 
 input = File.read("input")
-defs, _my_ticket, other_tickets = input.split("\n\n")
+defs, my_ticket, other_tickets = input.split("\n\n")
 defs.split("\n").each do |line|
   scan_result = line.scan(/(.+): (\d+-\d+) or (\d+-\d+)/)
   name, range1, range2 = scan_result.first
@@ -11,9 +11,48 @@ defs.split("\n").each do |line|
   fields[name] = [range1, range2]
 end
 
-invs = other_tickets.split("\n").drop(1).map do |ticket|
+invalid_fields_sum = 0
+valid_tickets = other_tickets.split("\n").drop(1).select do |ticket|
   nums = ticket.split(",").map(&:to_i)
-  nums.reject {|num| fields.values.any?{|f| f.any? {|r| r.include?(num)}}}
+  invs = nums.reject {|num| fields.values.any?{|f| f.any? {|r| r.include?(num)}}}
+  invalid_fields_sum += invs.sum
+  invs.empty?
 end
 
-puts invs.flatten.sum
+puts invalid_fields_sum
+
+require 'set'
+
+possible_fields = {}
+valid_tickets.each do |ticket|
+  nums = ticket.split(",").map(&:to_i)
+  nums.each_with_index do |num, idx|
+    valid_labels = Set.new
+    fields.each do |label, ranges|
+      valid_labels << label if ranges.any? {|r| r.include?(num)}
+    end
+    possible_fields[idx] = possible_fields[idx] ? possible_fields[idx] & valid_labels : valid_labels
+  end
+end
+
+def_fields = {}
+loop do
+  singles = possible_fields.select {|_,set| set.size == 1}
+  break if singles.empty?
+  singles.each do |idx, single|
+    single = single.to_a.first
+    def_fields[idx] = single
+    possible_fields.each {|_, s| s.delete(single)}
+  end
+end
+
+departure_fields = def_fields.select{|idx, label| label =~ /departure/}
+
+my_ticket_vals = my_ticket.split("\n").last.split(",").map(&:to_i)
+
+result = 1
+departure_fields.each do |idx, _|
+  result *= my_ticket_vals[idx]
+end
+
+puts result
